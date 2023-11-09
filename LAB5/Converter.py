7a4060e1dbc156ccf6be0eab1901069cf6d2784c
Darah Via <ddmoscoso@up.edu.ph>
2023-11-09 09:41:42 +0800

    k na

--- LAB5/Converter.py
+++ LAB5/Converter.py
@@ -59,15 +59,14 @@ def postfixToInfix(expression):
             operand1 = stack.pop().getValue()
             stack.push(f'({operand1} {char} {operand2})')
 
-    return stack.top().getValue() if stack else ""
+    return stack.top().getValue() if stack else ""              #top of the stack contains final
 
 # Main program
 while True:
     userChoice = input("1: Infix to Postfix\n2: Postfix to Infix\nChoice: ")
 
     if userChoice == '1' or userChoice == '2':
-        expression = input("Enter an expression: ").strip()
-        print(expression)
+        expression = input("Enter an expression: ")
 
         if userChoice == '1':
             postfix = infixToPostfix(expression)
